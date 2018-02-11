b10ccd25b3f5741382b3666ac5547e37afe97764
cardoso <matheus@cardo.so>
2018-02-11 16:39:11 -0300

    feat: custom SplitViewController (always show master)

---
+++ ReduxMovieDB/SplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplitViewController.swift
+//  ReduxMovieDB
+//
+//  Created by Matheus Cardoso on 2/11/18.
+//  Copyright © 2018 Matheus Cardoso. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        preferredDisplayMode = .allVisible
+    }
+}
+
