244dc3422431af3258cc433d03fcb1b575880f87
cardoso <matheus@cardo.so>
2018-02-11 16:57:49 -0300

    feat: UITableViewDataSource in MoviesViewController

---
+++ ReduxMovieDB/MoviesViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MoviesViewController.swift
+//  ReduxMovieDB
+//
+//  Created by Matheus Cardoso on 2/11/18.
+//  Copyright © 2018 Matheus Cardoso. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MoviesViewController: UIViewController {
+    @IBOutlet weak var moviesTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension MoviesViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
